c44f99f8f5b6d19a98bcbe7b7faee27f81814a81
Ali Saadati <saadati944@gmail.com>
2021-04-28 21:49:57 +0430

    add page to add tags

--- MvcNews/Controllers/HomeController.cs
+++ MvcNews/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MvcNews.Data;
 using MvcNews.Models;
 
 namespace MvcNews.Controllers
@@ -12,10 +13,12 @@ namespace MvcNews.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly NewsDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, NewsDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -33,5 +36,38 @@ namespace MvcNews.Controllers
         {
             return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
         }
+        
+        
+        // tags
+        [HttpGet]
+        public IActionResult Tags()
+        {
+            return View(_context.Tags.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult CreateTag()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateTag(Tag newtag)
+        {
+            _context.Tags.Add(newtag);
+            _logger.LogInformation(newtag.Id.ToString());
+            _logger.LogInformation(newtag.Name);
+            _context.SaveChanges();
+            _logger.LogInformation(_context.Tags.ToList().Count.ToString());
+            return RedirectToAction("Tags", "Home");
+        }
+        
+        // [HttpGet]
+        // public IActionResult TagPosts(string? name)
+        // {
+        //     if (String.IsNullOrEmpty(name))
+        //         return Tags();
+        //
+        //     return View();
+        // }
     }
 }
