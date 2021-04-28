de98669ef236101b8b1bd8cc70e081735f55a105
Ali Saadati <saadati944@gmail.com>
2021-04-28 21:27:30 +0430

    add newsdbcontext

---
+++ MvcNews/Data/NewsDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MvcNews.Models;
+
+namespace MvcNews.Data
+{
+    public class NewsDbContext : DbContext
+    {
+        public NewsDbContext (DbContextOptions<NewsDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
