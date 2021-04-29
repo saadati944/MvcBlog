d0885dba80e8887f3017c4cf8bc92e11b00dc0d0
Ali Saadati <saadati944@gmail.com>
2021-04-29 12:20:16 +0430

    add signup action and page

--- MvcNews/Controllers/AccountController.cs
+++ MvcNews/Controllers/AccountController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -21,6 +22,58 @@ namespace MvcNews.Controllers
         {
             await _signInManager.SignOutAsync();
             return RedirectToAction("Index", "Home");
+        }
+
+        
+        // register new user
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpModel newUser)
+        {
+            if(!ModelState.IsValid)
+                return View();
+
+            Models.User user = new User
+            {
+                UserName = newUser.UserName,
+                Email = newUser.Email
+            };
+            var result = await _userManager.CreateAsync(user, newUser.Password);
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError(string.Empty, error.Description);
+                return View();
+            }
+            
+            //todo: redirect to login page after creating a login page
+            return RedirectToAction("Index", "Home");
+        }
+
+        public class SignUpModel
+        {
+            [StringLength(130, ErrorMessage = "Max username len is 130 characters.")]
+            public string UserName { get; set; }
+            
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Email")]
+            public string Email { get; set; }
+
+            [Required]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Display(Name = "Confirm password")]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
         }
     }
 }
